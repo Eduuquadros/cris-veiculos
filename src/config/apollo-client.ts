6547302cb9b89c2cc411d5bc2f2671fe0e40857f
Eduux <eduardosantanaquadros@gmail.com>
2023-04-19 19:48:24 -0300

    *

--- src/config/apollo-client.ts
+++ src/config/apollo-client.ts
@@ -1,9 +1,25 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  DefaultOptions,
+  InMemoryCache,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
   uri: "https://api-sa-east-1.hygraph.com/v2/clgkgxive34uq01t28bc66f74/master",
 });
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
 
 const authLink = setContext((_, { headers }) => {
   const token = process.env.TOKEN;
@@ -18,7 +34,8 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache({ resultCaching: true }),
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
 });
 
 export default client;
